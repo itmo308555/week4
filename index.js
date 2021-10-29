a44f3d4837e8723356b2f967ac65c3be3d82587b
itmo308555 <92982321+itmo308555@users.noreply.github.com>
2021-10-29 16:13:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(bodyParser.text())
-app.use(cors())
+#app.use(cors())
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+
+
 app.post('/result4/', (req, res) => {
+    
+    
+     res.writeHead(200,{
+              'Content-Type':'application/json',
+              ...CORS,
+          })
+    
+    
     const xTest = req?.headers?.['x-test']
     const xBody = req?.body
     res.json({ message: 'id71293382', "x-result": xTest, "x-body": xBody })
 })
 app.get('/result4/', (req, res) => {
+    
+    
+     res.writeHead(200,{
+              'Content-Type':'application/json',
+              ...CORS,
+          })
+    
     const xTest = 'baovn'
     const xBody = 'abc'
     res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
