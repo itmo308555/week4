800b6469bbc86640573b5307eddec8742386259c
itmo308555 <92982321+itmo308555@users.noreply.github.com>
2021-10-29 15:32:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const PORT = process.env.PORT || 5000
-const app = express()
-app.use(bodyParser.text())
-app.use(cors())
-app.post('/result4/', (req, res) => {
-    const xTest = req?.headers?.['x-test']
-    const xBody = req?.body
-    res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
-})
-app.get('/result4/', (req, res) => {
-    const xTest = req?.headers?.['x-test']
-    const xBody = 'abc'
-    res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
-})
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
+import http from 'http';
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+const server = http.createServer(async(req,res)=>{
+      if(req.url === '/result4/'){
+          
+          res.writeHead(200,{
+              'Content-Type':'application/json',
+              ...CORS,
+          })
+          let data = '';
+          await req.on('data', function(chunk){
+            data += chunk;
+            }).on('end', () => {
+          })
+          res.write(JSON.stringify({
+            "message":"itmo307709",
+            "x-result":headers,
+            "x-body":data
+            }
+            ))
+      }
+      res.end()
+      
+  });
+  server.listen(4321,()=>{
+      console.log('Server is running')});
