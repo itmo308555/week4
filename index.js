bc632a0f0bad9b9504f8e51f74f6d825d9da13e0
itmo308555 <92982321+itmo308555@users.noreply.github.com>
2021-10-29 16:48:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/result4/', (req, res) => {
    
     const xTest = req?.headers?.['x-test']
     const xBody = req?.body
-    res.json({ message: 'id71293382', "x-result": xTest, "x-body": xBody })
+    res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
 })
 app.get('/result4/', (req, res) => {
     
@@ -31,14 +31,14 @@ app.get('/result4/', (req, res) => {
     const xTest = 'baovn'
     const xBody = 'abc'
     
-  /*  res.write(JSON.stringify({
+   res.write(JSON.stringify({
             "message":"itmo308555",
             "x-result":xTest,
             "x-body":xBody
             }
-            )) */
+            ))
     
-     res.text({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
+     //res.text({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
     
     //res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
 })
