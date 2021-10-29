990ef30935cb0665459fd0f007ed748bb565dc08
baovn-2976 <74024040+baovn-2976@users.noreply.github.com>
2021-10-29 15:22:22 +0300

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+const app = express()
+app.use(bodyParser.text())
+app.use(cors())
+app.post('/result4/', (req, res) => {
+    const xTest = req?.headers?.['x-test']
+    const xBody = req?.body
+    res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
+})
+app.get('/result4/', (req, res) => {
+    const xTest = req?.headers?.['x-test']
+    const xBody = 'abc'
+    res.json({ message: 'itmo308555', "x-result": xTest, "x-body": xBody })
+})
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
